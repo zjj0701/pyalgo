432b84ec4b787cd6ff3264fc767d3434ace881c0
zjj <544562474@qq.com>
2024-11-07 21:02:46 +0800

    format code

--- min_cost_ladder.py
+++ min_cost_ladder.py
@@ -1,7 +1,6 @@
 from typing import List
 
 
-
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         index_0, index_1 = cost[0], cost[1]
@@ -11,7 +10,8 @@ class Solution:
         dp[1] = index_1
         for i in range(2, num_ladder):
             dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i]
-        return min(dp[num_ladder-1],dp[num_ladder-2])
-s = Solution()
-print(s.minCostClimbingStairs([10,15,20]))
+        return min(dp[num_ladder - 1], dp[num_ladder - 2])
 
+
+s = Solution()
+print(s.minCostClimbingStairs([10, 15, 20]))
